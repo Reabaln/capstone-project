851d3626b8fb0f328fe4f96c3b65e9738bdfd1ed
Reab <63632708+Reabaln@users.noreply.github.com>
2020-10-29 18:45:58 +0300

    Update checkout_test.js

--- e2e-js-test/checkout_test.js
+++ e2e-js-test/checkout_test.js
@@ -45,9 +45,9 @@
 
     // go to the shopping cart
     casper.then(function() {
-      this.waitForText("2 item(s) in cart", function() {
+      this.waitForText(" item(s) in cart", function() {
         test.pass("cart is updated with one product");
-        this.clickLabel("2 item(s) in cart");
+        this.clickLabel(" item(s) in cart");
       }, function() {
         test.fail("cart was not updated");
       }, 3000);
