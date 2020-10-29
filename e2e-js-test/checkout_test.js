712d11c8a84171d0d32b88b184f54ba92215bb94
Ubuntu <ubuntu@ip-10-0-1-85.me-south-1.compute.internal>
2020-10-29 13:55:38 +0000

    testing

--- e2e-js-test/checkout_test.js
+++ e2e-js-test/checkout_test.js
@@ -12,8 +12,8 @@
       casper.waitUntilVisible("#login-modal", function() {
       test.assertVisible("#login-modal", "user is presented with the login dialogue");
       this.fill("#login-modal form", {
-        "username": "my-username",
-        "password": "password"
+        "username": "Eve_Berger",
+        "password": "eve"
       }, false);
       }, function() {
         test.fail("login dialogue never showed up");
--- e2e-js-test/login_test.js
+++ e2e-js-test/login_test.js
@@ -11,8 +11,8 @@
       casper.waitUntilVisible("#login-modal", function() {
         test.assertVisible("#login-modal", "user is presented with the login dialogue");
         this.fill("#login-modal form", {
-          "username": "my-username",
-          "password": "password"
+          "username": "Eve_Berger",
+          "password": "eve"
         }, false);
       }, function() {
         test.fail("login dialogue never showed up");
@@ -21,7 +21,7 @@
 
     casper.then(function() {
       this.click("#login-modal form button.btn.btn-primary");
-      this.waitForText("Logged in as Rehab N", function() {
+      this.waitForText("Logged in as", function() {
         test.pass("user is logged in");
       }, function() {
         test.fail("user login failed");
